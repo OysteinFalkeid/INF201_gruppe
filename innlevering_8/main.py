de36bd8e2aaf5171fa6d91fccabea8269cacca32
Eirik <eamenty@gmail.com>
2024-11-12 17:38:47 +0100

    Completed task 0

--- innlevering_8/main.py
+++ innlevering_8/main.py
@@ -37,45 +37,104 @@ from typing import Optional, Union
 
 
 #-------------------------------------------------------------------------------------------------------------------
-# Task 0
+# Task 0 DONE
+
+# class Student(ABC):
+#     def __init__(self, name, student_id):
+#         self._name = name
+#         self._student_id = student_id
+#         super().__init__()
+#     @abstractmethod
+#     def __str__(self):
+#         #string = 'Name: '+str(self._name)+'\nStudent ID: '+str(self._student_id)
+#         #return string
+#         pass
+
+# class DataScienceStudent(Student):
+#     def __init__(self, name : str, student_id : Union[str, int]):
+#         super().__init__(name, student_id)
+#     def __str__(self):
+#         #string = super().__str__()
+#         string = f"Data Science Student: [{self._student_id}] - [{self._name}]"
+#         return string
+    
+# class BiologyStudent(Student):
+#     def __init__(self, name : str, student_id : Union[str, int]):
+#         super().__init__(name, student_id)
+#     def __str__(self):
+#         #string = super().__str__()
+#         string = f"Biology Student: [{self._name}], ID: [{self._student_id}]"
+#         return string   
+
+# class StudentFactory:
+#     def __init__(self):
+#         #self._student_types = {"ds": DataScienceStudent, "bio": BiologyStudent}
+#         self._student_types = {}
+#     def register(self, key, student_class):
+#         self._student_types[key] = student_class
+#     def __call__(self, key, name, student_id):
+#         if key not in self._student_types:
+#             return f"Student type {key} not registered"
+#         else:
+#             return self._student_types[key](name, student_id)
+    
+# factory = StudentFactory()
+
+# factory.register("ds", DataScienceStudent)
+# factory.register("bio", BiologyStudent)
+
+# student0 = factory("ds", "Eirik", 100)
+# student1 = factory("bio", "Mathias", 101)
+
+# print(student0)
+# print(student1)
+
+#-------------------------------------------------------------------------------------------------------------------
+# Task 0 EXPERIMENTAL
 
 class Student(ABC):
     def __init__(self, name, student_id):
-        self._name = name
-        self._student_id = student_id
-        super().__init__()
+        self.name = name
+        self.student_id = student_id
     @abstractmethod
     def __str__(self):
-        #string = 'Name: '+str(self._name)+'\nStudent ID: '+str(self._student_id)
-        #return string
-        pass
-
-class DataScienceStudent(Student):
-    def __init__(self, name : str, student_id : Union[str, int]):
-        super().__init__(name, student_id)
-    def __str__(self):
-        #string = super().__str__()
-        string = f"Data Science Student: [{self._student_id}] - [{self._name}]"
-        return string
+        return f"Name: {self.name}, StudentID: {self.student_id}"
     
-class BiologyStudent(Student):
-    def __init__(self, name : str, student_id : Union[str, int]):
+class DataScienceStudent(Student):
+    def __init__(self, name, student_id):
         super().__init__(name, student_id)
     def __str__(self):
-        #string = super().__str__()
-        string = f"Biology Student: [{self._name}], ID: [{self._student_id}]"
-        return string   
+        string = super().__str__()
+        string += f", Course: Data Science"
+        return string
+
+class BiologyStudent(Student):
+    def __init__(self, name, student_id):
+        super().__init__(name, student_id)
+    def __str__(self):
+        string = super().__str__()
+        string += f", Course: Biology"
+        return string
 
 class StudentFactory:
-    def __init__(self, key):
-        self._key = key
+    def __init__(self):
+        self.student_types = {}
     def register(self, key, student_class):
+        self.student_types[key] = student_class
+    def __call__(self, key, name, student_id):
+        if key not in self.student_types:
+            return f"Error: {key} not in registry"
+        else:
+            return self.student_types[key](name, student_id)
 
+fac = StudentFactory()
+fac.register("ds", DataScienceStudent)
+fac.register("bio", BiologyStudent)
 
-a = BiologyStudent('Eirik', 10)
-b = DataScienceStudent('Øystein', 11)
-print(a)
-print(b)
+student0 = fac("ds", "Eirik", 10)
+
+print(student0)
+
 #-------------------------------------------------------------------------------------------------------------------
 # Task 1
 
