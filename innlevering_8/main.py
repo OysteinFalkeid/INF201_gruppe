5d41c3a40b81ca956ab19636d4cdaa99319a0d85
OysteinFalkeid <oystein.fal@gmail.com>
2024-11-12 13:59:51 +0100

    classes defined

--- innlevering_8/main.py
+++ innlevering_8/main.py
@@ -33,37 +33,67 @@ print()
 import meshio
 from pathlib import Path
 from abc import ABC, abstractmethod
+from typing import Optional
 
 #-------------------------------------------------------------------------------------------------------------------
 # Task 2
 
 
-class mesh:
-    def __init__(self):
+class Mesh:
+    def __init__(self, path: Optional[str] = None):
         self._point = []
         self._cell = []
+        if path:
+            self.read(path)
         
     def read(self, path: str) -> object:
         msh = meshio.read(path)
         return msh
             
 
-class point:
-    def __init__(self, x: float, y: float):
+class Point:
+    def __init__(self, index: int, x: float, y: float):
+        self._index = index
         self._x = 0.0
         self._y = 0.0
         if x and y:
             self._x = x
             self._y = y
-        
-            
-        
+        else:
+            print('point init missing coordinates')
         
 
-class cell(ABC):
-    def __init__(self):
+class Cell(ABC):
+    def __init__(self, index: int):
+        self._index = index
+        self._point = []
         super().__init__()
+    
+    @abstractmethod
+    def __str__(self):
+        string = 'Index: ' + str(self._index)
+        return string
+    
+
+class Triangle(Cell):
+    def __init__(self, index, point_1, point_2, point_3):
+        super().__init__(index)
+        self._point = [point_1, point_2, point_3]
+    
+    def __str__(self):
+        string = super().__str__()
+        string += f', Points: ({self._point[0]}, {self._point[1]}, {self._point[2]})'
+        return string
         
+class Line(Cell):
+    def __init__(self, index, point_1, point_2):
+        super().__init__(index)
+        self._point = [point_1, point_2]
+    
+    def __str__(self):
+        string = super().__str__()
+        string += f', Points: ({self._point[0]}, {self._point[1]})'
+        return string
 
 
 
