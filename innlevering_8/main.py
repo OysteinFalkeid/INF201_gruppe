8fa369bbb49deb99c942b60bc8d74d15f07056d7
Eirik <eamenty@gmail.com>
2024-11-12 15:21:37 +0100

    Tasks 1,2

--- innlevering_8/main.py
+++ innlevering_8/main.py
@@ -32,23 +32,52 @@ print()
 
 import meshio
 from pathlib import Path
+from abc import ABC, abstractmethod
+from typing import Optional, Union
 
 
 #-------------------------------------------------------------------------------------------------------------------
 # Task 0
 
+class Student(ABC):
+    def __init__(self, name, student_id):
+        self._name = name
+        self._student_id = student_id
+        super().__init__()
+    @abstractmethod
+    def __str__(self):
+        #string = 'Name: '+str(self._name)+'\nStudent ID: '+str(self._student_id)
+        #return string
+        pass
 
+class DataScienceStudent(Student):
+    def __init__(self, name : str, student_id : Union[str, int]):
+        super().__init__(name, student_id)
+    def __str__(self):
+        #string = super().__str__()
+        string = f"Data Science Student: [{self._student_id}] - [{self._name}]"
+        return string
+    
+class BiologyStudent(Student):
+    def __init__(self, name : str, student_id : Union[str, int]):
+        super().__init__(name, student_id)
+    def __str__(self):
+        #string = super().__str__()
+        string = f"Biology Student: [{self._name}], ID: [{self._student_id}]"
+        return string   
 
+a = BiologyStudent('Eirik', 10)
+print(a)
 #-------------------------------------------------------------------------------------------------------------------
 # Task 1
 
-msh_name = Path.cwd() / Path('simple.msh')
-msh = meshio.read(msh_name)
-points = msh.points  # mesh points
-cells = msh.cells    # mesh cells
-cell = cells[1].data[222]
-print(cell)
-print(cells[1].type)
+# msh_name = Path.cwd() / Path('simple.msh')
+# msh = meshio.read(msh_name)
+# points = msh.points  # mesh points
+# cells = msh.cells    # mesh cells
+# cell = cells[1].data[222]
+# print(cell)
+# print(cells[1].type)
 
 
 
