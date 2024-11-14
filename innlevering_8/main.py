558405eddc9c3055aa7132b0455f2f6cf89e2b30
Eirik <eamenty@gmail.com>
2024-11-14 12:04:29 +0100

    Fixed read

--- innlevering_8/main.py
+++ innlevering_8/main.py
@@ -147,86 +147,19 @@ class Line(Cell):
         string += f'\n  The cell is constructed from Points: ({self._points[0]}, {self._points[1]})'
         return string
 
-class Mesh:
-    def __init__(self, path: Optional[str] = None):
-        '''
-        Creates a class containing lists of points and cells.
-        The class has a read function that reads from .msh file if path exists
-        
-        Args:
-            path (str): The path to the .msh file containing the mesh
-            
-        Examples:
-            >>> Mesh(my/path.msh)
-            object containing lists based on my/path.msh
-            >>> Mesh()
-            Emptey object
-        '''
-        self._points = []
-        self._cells = []
-        if path:
-            self.mesh = self.read(path)
-        
-    def read(self, path: str) -> object:
-        '''
-        Reads from file at path. Saves file content as lists.
-        
-        Args:
-            path (str): The path to the .msh file containing the mesh
-            
-        Examples:
-            >>> read(my/path.msh)
-            Saves file content as lists at points and cells
-        '''
-        self._physical_names = []
-        with open(path, 'r') as file:
-            while True:
-                line = file.readline().strip()
-                if line == '':
-                    break
-                elif line == '$MeshFormat':
-                    line = file.readline().strip().split(' ')
-                    while line != '$EndMeshFormat':
-                        line = [float(line[0]), int(line[1]), int(line[2])]
-                        self._format = line
-                        line = file.readline().strip()
-                elif line == '$PhysicalNames':
-                    self._num_physical_names = file.readline().strip()
-                    line = file.readline().strip()
-                    while line != '$EndPhysicalNames':
-                        line = line.split(' ')
-                        line = [int(line[0]), int(line[1]), line[2].strip('"')]
-                        self._physical_names.append(line)
-                        line = file.readline().strip()
-                elif line == '$Nodes':
-                    line = file.readline().strip()
-                    self._num_nodes = int(line)
-                    line = file.readline().strip().split(' ')
-                    while line[0] != '$EndNodes':
-                        self._points.append(Point(int(line[0]), x=float(line[1]), y=float(line[2]), z=float(line[3])))
-                        line = file.readline().strip().split(' ')
-                elif line == '$Elements':
-                    line = file.readline().strip()
-                    self._num_elements = line
-                    line = file.readline().strip().split(' ')
-                    while len(line) < 8:
-                        self._cells.append(Line(int(line[0]), element_type=int(line[1]), num_tags=int(line[2]), physical_entity=int(line[3]), elementary_entity=int(line[4]), point_1=int(line[5]), point_2=int(line[6])))
-                        line = file.readline().strip().split(' ')
-                    while line[0] != '$EndElements':
-                        self._cells.append(Triangle(int(line[0]), element_type=int(line[1]), num_tags=int(line[2]), physical_entity=int(line[3]), elementary_entity=int(line[4]), point_1=int(line[5]), point_2=int(line[6]), point_3=int(line[7])))
-                        line = file.readline().strip().split(' ')
-                
-    
-    @property
-    def points(self):
-        return self._points
-    
-    @property
-    def cells(self):
-        return self._cells
-    
-    def __str__(self):
-        return f'Mesh with {len(self.points)} points and {len(self.cells)} cells'
+class MeshFactory:
+    def __init__(self):
+        self._shape = {7: Line, 8: Triangle}
+    def register(self, key, shape_class):
+        self._shape[key] = shape_class
+    def __call__(self, list):
+        length = len(list)
+        if length not in self._shape:
+            return f"error"
+        else:
+            list = [int(i) for i in list]
+            return self._shape[length](list[0], list[1], list[2], list[3], list[4], list[5:])
+
 
 # class Mesh:
 #     def __init__(self, path: Optional[str] = None):
@@ -342,6 +275,7 @@ class Mesh:
         '''
         self._physical_names = []
         with open(path, 'r') as file:
+            factory = MeshFactory()
             while True:
                 line = file.readline().strip()
                 if line == '':
@@ -371,13 +305,19 @@ class Mesh:
                     line = file.readline().strip()
                     self._num_elements = line
                     line = file.readline().strip().split(' ')
-                    while len(line) < 8:
-                        temp_list = line[5:]
-                        self._cells.append(Line(int(line[0]), element_type=int(line[1]), num_tags=int(line[2]), physical_entity=int(line[3]), elementary_entity=int(line[4]), temp_list[5:]))
-                        line = file.readline().strip().split(' ')
                     while line[0] != '$EndElements':
-                        self._cells.append(Triangle(int(line[0]), element_type=int(line[1]), num_tags=int(line[2]), physical_entity=int(line[3]), elementary_entity=int(line[4]), point_1=int(line[5]), point_2=int(line[6]), point_3=int(line[7])))
+                        self._cells.append(factory(line))
                         line = file.readline().strip().split(' ')
+    @property
+    def points(self):
+        return self._points
+    
+    @property
+    def cells(self):
+        return self._cells
+    
+    def __str__(self):
+        return f'Mesh with {len(self.points)} points and {len(self.cells)} cells'
 
 path = Path.cwd() / Path('simple.msh')
 mesh = Mesh(path)
@@ -388,19 +328,6 @@ print()
 
 for cell in mesh.cells:
     print(cell)
-
-class MeshFactory:
-    def __init__(self):
-        self._shape = {7: Line, 8: Triangle}
-    def register(self, key, shape_class):
-        self._shape[key] = shape_class
-    def __call__(self, list):
-        length = len(list)
-        if length not in self._shape:
-            return f"error"
-        else:
-            list = [int(i) for i in list]
-            return self._shape[length](list[0], list[1], list[2], list[3], list[4], list[5:])
 
 test_fac = MeshFactory()
 list_0 = [1, 2, 3, 4, 5, 6, 7, 8]
