996b2dda64c08d3b735c88e36805f6307c6e0632
OysteinFalkeid <oystein.fal@gmail.com>
2024-11-14 08:13:06 +0100

    reddy to merge back

--- innlevering_8/main.py
+++ innlevering_8/main.py
@@ -15,7 +15,7 @@ from pathlib import Path
 file = Path.cwd() / Path('terminal.txt')
 sys.stdout = open(file, 'w',encoding='utf-8')
 
-print('innlevering 6')
+print('innlevering 8')
 print()
 
 #-------------------------------------------------------------------------------------------------------------------
@@ -39,61 +39,15 @@ from typing import Optional
 # Task 2
 
 
-class Mesh:
-    def __init__(self, path: Optional[str] = None):
-        '''
-        Creates a class containing lists of points and cells.
-        The class has a read function that reads from .msh file if path exists
-        
-        Args:
-            path (str): The path to the .msh file containing the mesh
-            
-        Examples:
-            >>> Mesh(my/path.msh)
-            object containing lists based on my/path.msh
-            >>> Mesh()
-            Emptey object
-        '''
-        self._points = []
-        self._cells = []
-        if path:
-            self.mesh = self.read(path)
-        
-    def read(self, path: str) -> object:
-        '''
-        Reads from file at path. Saves file content as lists.
-        
-        Args:
-            path (str): The path to the .msh file containing the mesh
-            
-        Examples:
-            >>> read(my/path.msh)
-            Saves file content as lists at points and cells
-        '''
-        msh = meshio.read(path)
-        
-        for point in msh.points:
-            pass
-        return msh
-    
-    @property
-    def points(self):
-        return self._points
-    
-    @property
-    def cells(self):
-        return self._cells
+
 
 class Point:
-    def __init__(self, index: int, x: float, y: float):
+    def __init__(self, index: int, x: float, y: float, z: float):
         self._index = index
-        self._x = 0.0
-        self._y = 0.0
-        if x and y:
-            self._x = x
-            self._y = y
-        else:
-            print('point init missing coordinates')
+        
+        self._x = x
+        self._y = y
+        self._z = z
     
     @property
     def index(self):
@@ -106,6 +60,13 @@ class Point:
     @property
     def y(self):
         return self._y
+    
+    @property
+    def z(self):
+        return self._z
+    
+    def __str__(self):
+        return f'Point: {self.index} at (x, y, z): ({self.x}, {self.y}, {self.z})'
         
 
 class Cell(ABC):
@@ -148,8 +109,80 @@ class Line(Cell):
         string += f', Points: ({self._point[0]}, {self._point[1]})'
         return string
 
+class Mesh:
+    def __init__(self, path: Optional[str] = None):
+        '''
+        Creates a class containing lists of points and cells.
+        The class has a read function that reads from .msh file if path exists
+        
+        Args:
+            path (str): The path to the .msh file containing the mesh
+            
+        Examples:
+            >>> Mesh(my/path.msh)
+            object containing lists based on my/path.msh
+            >>> Mesh()
+            Emptey object
+        '''
+        self._points = []
+        self._cells = []
+        if path:
+            self.mesh = self.read(path)
+        
+    def read(self, path: str) -> object:
+        '''
+        Reads from file at path. Saves file content as lists.
+        
+        Args:
+            path (str): The path to the .msh file containing the mesh
+            
+        Examples:
+            >>> read(my/path.msh)
+            Saves file content as lists at points and cells
+        '''
+        self._physical_names = []
+        with open(path, 'r') as file:
+            while True:
+                line = file.readline().strip()
+                if line == '':
+                    break
+                elif line == '$MeshFormat':
+                    while line != '$EndMeshFormat':
+                        line = file.readline().strip()
+                        self._format = line
+                elif line == '$PhysicalNames':
+                    while line != '$EndPhysicalNames':
+                        line = file.readline().strip()
+                        self._physical_names.append(line)
+                elif line == '$Nodes':
+                    line = file.readline().strip()
+                    self._num_nodes = int(line)
+                    line = file.readline().strip().split(' ')
+                    while line[0] != '$EndNodes':
+                        self._points.append(Point(int(line[0]), float(line[1]), float(line[2]), float(line[3])))
+                        line = file.readline().strip().split(' ')
+                elif line == '$Elements':
+                    line = file.readline().strip()
+                    self._num_elements = line
+                    line = file.readline().strip().split(' ')
+                    while len(line) < 8:
+                        self._cells.append(Line(int(line[0]), float(line[5]), float(line[6])))
+                        line = file.readline().strip().split(' ')
+                    while line[0] != '$EndElements':
+                        self._cells.append(Line(int(line[0]), float(line[5]), float(line[6])))
+                        line = file.readline().strip().split(' ')
+                
+    
+    @property
+    def points(self):
+        return self._points
+    
+    @property
+    def cells(self):
+        return self._cells
+
 trekant = Triangle(1, 1, 2, 3)
 
 path = Path.cwd() / Path('simple.msh')
-mesh = meshio.read(path)
-print(mesh)
+mesh = Mesh(path)
+print(mesh.points)
